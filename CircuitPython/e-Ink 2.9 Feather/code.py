2748b86fcc1aa0e671cb9dc8041c183839806279
Poent <Poent@users.noreply.github.com>
2022-09-29 10:33:07 -0600

    Update code.py

--- CircuitPython/e-Ink 2.9 Feather/code.py
+++ CircuitPython/e-Ink 2.9 Feather/code.py
@@ -16,15 +16,23 @@ time.sleep(1)
 print("epd2in9 V2 Demo") 
 epd = epd2in9_V2.EPD()
 
+#initialize the display as documented
 print("init and Clear")
 epd.init()
+
+# clear the display with a simple alternating bit pattern 0101 0101 (0x55)
 epd.Clear(0x55) 
 time.sleep(5)
+
+# send a small display update to identify the corner it's writing from first (x/y orientation)
 epd.display([0x00] * 4)
-epd.Clear(0xFF) 
-time.sleep(30)
+time.sleep(10)
+
+#wipe the screen
 epd.Clear(0xFF)
 time.sleep(5)
+
+# power down as recommended
 print("Deep Sleep...")
 epd.sleep()
 
